4a2b76978ae2fe251f3c7a61ab410d24ea25b1d0
nshah <nshah@sigmasolve.net>
2021-04-07 20:13:07 +0530

    solved challange

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 from prettytable import PrettyTable
+import numpy as np
+import pandas as pd
 
 table = PrettyTable()
 table.field_names = ['id', 'name', 'price', 'expires']
+global df
 
-
-def display_table():
+def display_table(value):
     """
     Display product table
     """
 
-    table.add_row([1, 'First sample item', 1.23, 'JAN-01-2019'])
-    table.add_row([2, 'Second sample item', 2.34, 'JAN-02-2019'])
+    min_price = np.where(float(value[0]), float(value[0]), df["price"].min())
+    max_price = np.where(float(value[1]), float(value[1]), df["price"].max())
+    mask1 = df['price'] >= min_price
+    mask2 = df["price"] <= max_price
+    try:
+        expires_start = pd.to_datetime(value[2])
+        expires_stop = pd.to_datetime(value[3])
+        mask3 = df["expires"] >= expires_start
+        mask4 = df["expires"] <= expires_stop
+        data = df[(mask1 & mask2) & (mask3 & mask4)]
+    except Exception as e:
+        data = df[mask1 & mask2]
+        print(e)
+    for idx,row in data.iterrows():
+        table.add_row([row['id'], row['name'], row['price'], row['expires']])
     print(table)
     table.clear_rows()
 
@@ -23,10 +38,13 @@ def start():
 
     while True:
         value = input('> ')
-        if value == 'exit':
+        value = value.split(' ')
+        if value[0] == 'exit':
             break
-        display_table()
+        display_table(value)
 
 
 if __name__ == '__main__':
+    df = pd.read_csv('products.csv')
+    df['expires'] = pd.to_datetime(df['expires'])
     start()
